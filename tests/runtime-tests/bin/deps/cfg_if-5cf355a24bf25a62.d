0e31efd808e54215566929252f81470ca7a1ea08
Lochlan Wansbrough <>
2024-10-06 15:19:50 +0200

    remove bin

--- tests/runtime-tests/bin/deps/cfg_if-5cf355a24bf25a62.d
+++
@@ -1,7 +0,0 @@
-/Users/loch/Documents/Projects/wizard/rune/tests/runtime-tests/target/wasm32-wasi/debug/deps/libcfg_if-5cf355a24bf25a62.rmeta: /Users/loch/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/Users/loch/Documents/Projects/wizard/rune/tests/runtime-tests/target/wasm32-wasi/debug/deps/libcfg_if-5cf355a24bf25a62.rlib: /Users/loch/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/Users/loch/Documents/Projects/wizard/rune/tests/runtime-tests/target/wasm32-wasi/debug/deps/cfg_if-5cf355a24bf25a62.d: /Users/loch/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/Users/loch/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- tests/runtime-tests/bin/deps/wit_bindgen-c1da677be45e4b46.d
+++
@@ -1,6 +0,0 @@
-/Users/loch/Documents/Projects/wizard/rune/tests/runtime-tests/target/wasm32-wasi/debug/deps/libwit_bindgen-c1da677be45e4b46.rmeta: /Users/loch/.cargo/git/checkouts/wit-bindgen-f98ccff846621481/dea39d0/crates/guest-rust/src/lib.rs /Users/loch/.cargo/git/checkouts/wit-bindgen-f98ccff846621481/dea39d0/crates/guest-rust/src/pre_wit_bindgen_0_20_0.rs
-
-/Users/loch/Documents/Projects/wizard/rune/tests/runtime-tests/target/wasm32-wasi/debug/deps/wit_bindgen-c1da677be45e4b46.d: /Users/loch/.cargo/git/checkouts/wit-bindgen-f98ccff846621481/dea39d0/crates/guest-rust/src/lib.rs /Users/loch/.cargo/git/checkouts/wit-bindgen-f98ccff846621481/dea39d0/crates/guest-rust/src/pre_wit_bindgen_0_20_0.rs
-
-/Users/loch/.cargo/git/checkouts/wit-bindgen-f98ccff846621481/dea39d0/crates/guest-rust/src/lib.rs:
-/Users/loch/.cargo/git/checkouts/wit-bindgen-f98ccff846621481/dea39d0/crates/guest-rust/src/pre_wit_bindgen_0_20_0.rs:
